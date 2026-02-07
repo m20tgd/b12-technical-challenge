ef8a4f5e85a8ae2ed8f5a4d562e255a7207ff5e1
Tom Digby <tom.digby@gwtinsight.com>
2026-02-07 18:40:28 +0000

    Test GITHUB_RUN_ID const

--- trigger-api-call.py
+++ trigger-api-call.py
@@ -1,1 +1,17 @@
+import json
+
+
 print("Hello, World!")
+
+body = {
+    "name": "Tom Digby",
+    "email": "m20tgd@hotmail.co.uk",
+    "resume_link": "https://www.linkedin.com/in/tom-digby-71a819184/",
+    "repository_link": "https://github.com/m20tgd/b12-technical-challenge",
+    "action_run_link": GITHUB_RUN_ID,
+}
+
+payload = json.dumps(body, sort_keys=True,
+                     separators=(",", ":")).encode("utf-8")
+
+print(payload)
