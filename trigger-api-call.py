4cf42e38adda8c7b9ca4fff61dee7ecda58851f8
Tom Digby <tom.digby@gwtinsight.com>
2026-02-07 18:52:44 +0000

    Testing use of secrets

--- trigger-api-call.py
+++ trigger-api-call.py
@@ -4,14 +4,16 @@ import os
 
 print("Hello, World!")
 
-GITHUB_RUN_ID = os.getenv("GITHUB_RUN_ID")
+ACTION_RUN_URL = os.getenv("ACTION_RUN_URL")
+TEST = os.getenv("TEST")
+print(TEST)
 
 body = {
     "name": "Tom Digby",
     "email": "m20tgd@hotmail.co.uk",
     "resume_link": "https://www.linkedin.com/in/tom-digby-71a819184/",
     "repository_link": "https://github.com/m20tgd/b12-technical-challenge",
-    "action_run_link": GITHUB_RUN_ID,
+    "action_run_link": ACTION_RUN_URL,
 }
 
 payload = json.dumps(body, sort_keys=True,
